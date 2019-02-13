6136bd7af1d38ba28b6df97f9b57ef4aaf292a40
Oscar Arréhn Ågren <oscar093@live.se>
2019-02-13 07:01:50 +0100

    Add files via upload

--- src/Vector.h
+++ src/Vector.h
@@ -1,13 +1,15 @@
 /*
  * Vector.h
  *
- *  Created on: Nov 21, 2018
+ *  Created on: Dec 5, 2018
  *      Author: Oscar Arréhn Ågren
  */
 
 #ifndef VECTOR_H_
 #define VECTOR_H_
 #include <iostream>
+#include <cstring>
+#include <cassert>
 #include "VectorItt.h"
 
 template <typename T>
@@ -15,15 +17,16 @@ class Vector {
 	T* container = nullptr;
 	size_t sze;
 	size_t cpsty;
-	void invar();
+
 public:
+
+	void invar();
 	template<class TT> friend class VectorItt;
 	using iterator = VectorItt<T>;
 	Vector();
 	virtual ~Vector();
 	Vector(const Vector& other);
 	Vector(Vector&& other);
-	Vector(const char* other);
 	Vector& operator=(const Vector& other);
 	Vector& operator=(Vector&& other);
 	T& operator[](size_t i);
@@ -40,24 +43,336 @@ public:
 	void shrink_to_fit();
 	void push_back(T c);
 	void resize(size_t n);
-	friend bool operator==(const Vector& lhs,
-	               const Vector& other);
-	friend bool operator!=(const Vector& lhs,
-		               const Vector& other);
-	friend bool operator<=(const Vector& lhs,
-		               const Vector& other);
-	friend bool operator>=(const Vector& lhs,
-		               const Vector& other);
-	friend bool operator>(const Vector& lhs,
-		               const Vector& other);
-	friend bool operator>(const Vector& lhs,
-		               const Vector& other);
 
+	template <typename TT>
+	friend bool operator==(const Vector<TT>& lhs, const Vector<TT>& other){
+		if(other.sizee() != lhs.sizee()){
+			return false;
+		}
+		for (unsigned int i = 0; i < lhs.sizee(); ++i) {
+			if (lhs[i] != other[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template <typename TT>
+	friend bool operator!=(const Vector<TT>& lhs, const Vector<TT>& other){
+		return !(lhs == other);
+	}
+
+	template <typename TT>
+	friend bool operator<=(const Vector<TT>& lhs, const Vector<TT>& other){
+		return (lhs < other) || (lhs == other);
+	}
+
+	template <typename TT>
+	friend bool operator>=(const Vector<TT>& lhs, const Vector<TT>& other){
+		return !(lhs < other) || (lhs == other);
+	}
+
+	template <typename TT>
+	friend bool operator<(const Vector<TT>& lhs, const Vector<TT>& other){
+		for(unsigned int i = 0; i < other.sizee(); i++){
+			if (lhs[i] != other[i]) {
+					return lhs[i] < other[i];
+			}
+		}
+		return lhs != other;
+	}
+
+	template <typename TT>
+	friend bool operator>(const Vector<TT>& lhs, const Vector<TT>& other){
+		return  !(lhs < other);
+	}
+
+	template<typename TT>
+	friend void swap(Vector<TT>& lhs, Vector<TT>& rhs ){
+		TT* tmpptr = lhs.data();
+		size_t tmpsze = lhs.sizee();
+		size_t tmpcpsty = lhs.capacity();
+
+		lhs.container = rhs.data();
+		lhs.sze = rhs.sizee();
+		lhs.cpsty = rhs.capacity();
+
+		rhs.container = tmpptr;
+		rhs.sze = tmpsze;
+		rhs.cpsty = tmpcpsty;
+
+		lhs.invar();
+		rhs.invar();
+	}
+
+
+	/**
+	 * For testing
+	 */
 	friend std::ostream& operator<<(std::ostream& cout, const Vector& other){
 			for (size_t i = 0; i < other.sizee(); ++i)
 				cout << other[i];
 			return cout;
 	}
+	friend bool operator==(const Vector& lhs, const char* other){
+		unsigned int i = 0;
+		for(char* ptr = lhs.container; ptr != lhs.container + lhs.sze; ptr++){
+			if(*ptr != *(other + i)){
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	Vector(const char* other);
 };
+
+/************************************************************************************************
+ * Definition																					*
+ * **********************************************************************************************/
+
+template <typename T>
+void Vector<T>::invar(){
+	assert(Vector<T>::sze <= Vector<T>::cpsty);
+	assert(Vector<T>::sze >= 0);
+	assert(Vector<T>::cpsty >= 0);
+}
+
+
+template <typename T>
+Vector<T>::Vector() {
+	container = new T[2];
+	sze = 0;
+	cpsty = 2;
+	invar();
+}
+
+template <typename T>
+Vector<T>::~Vector() {
+	invar();
+	delete[] container;
+}
+
+template<typename T>
+Vector<T>::Vector(const char* other) {
+	this->sze = strlen(other);
+	cpsty = sze + sze;
+	container = new T[sze];
+	int i = 0;
+	for (T* tmpp = container; tmpp != container + sze; ++tmpp) {
+		*tmpp = other[i];
+		i++;
+	}
+	invar();
+}
+
+template<typename T>
+Vector<T>::Vector(const Vector& other) {
+	cpsty = other.capacity();
+	sze = other.sze;
+	container = new T[sze];
+	T* otherTptr = other.container;
+	for (T* charp = container; charp != container + sze; ++charp) {
+		*charp = *otherTptr;
+		otherTptr++;
+	}
+	invar();
+}
+
+template<typename T>
+Vector<T>::Vector(Vector&& other){
+	delete[] container;
+	container = other.container;
+	other.container = nullptr;
+	cpsty = other.cpsty;
+	sze = other.sze;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector& other){
+	invar();
+	if(other.sze > cpsty){
+	sze = other.sze;
+	cpsty = other.capacity();
+	T* tptr = new T[cpsty];
+	int i = 0;
+	for (T* tmpp = tptr; tmpp != tptr + sze; ++tmpp) {
+		*tmpp = other[i];
+		i++;
+	}
+	delete[] container;
+	container = tptr;
+	} else {
+		int i = 0;
+		for (T* tmpp = container; tmpp != container + other.sizee(); ++tmpp) {
+			*tmpp = other[i];
+			i++;
+		}
+		sze = other.sizee();
+	}
+	invar();
+	return *this;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(Vector&& other){
+	invar();
+	if (container != nullptr) {
+		delete[] container;
+	}
+	sze = other.sze;
+	cpsty = other.capacity();
+	container = new T[cpsty];
+	int i = 0;
+	for (T* tmpp = container; tmpp != container + sze; ++tmpp) {
+		*tmpp = *(other.container + i);
+		i++;
+	}
+	invar();
+	return *this;
+}
+
+template <typename T>
+void Vector<T>::reserve(size_t n){
+	if (n > cpsty) {
+		T* cont = new T[n];
+		int i = 0;
+		for (T* tmpp = container; tmpp < container + sze; ++tmpp) {
+			*(cont + i) = *tmpp;
+			i++;
+		}
+		delete[] container;
+		container = cont;
+		cpsty = n;
+		invar();
+	}
+}
+template <typename T>
+T& Vector<T>::operator[](size_t i){
+	return container[i];
+}
+
+template <typename T>
+T& Vector<T>::at(size_t i){
+	if (i >= 0 && i < sze) {
+			return container[i];
+		} else {
+			throw std::out_of_range("Cannot have negative parameter or bigger than size()");
+		}
+}
+
+template <typename T>
+const T& Vector<T>::operator[](size_t i) const{
+	return container[i];
+}
+
+template <typename T>
+const T& Vector<T>::at(size_t i) const{
+	if (i >= 0 && i < sze) {
+			return container[i];
+		} else {
+			throw std::out_of_range("Cannot have negative parameter or bigger than size()");
+		}
+}
+
+template <typename T>
+T* Vector<T>::data() noexcept{
+	return container;
+}
+
+template <typename T>
+const T* Vector<T>::data() const noexcept{
+	return container;
+}
+
+template <typename T>
+typename Vector<T>::iterator Vector<T>::begin() const{
+	VectorItt<T> vitt = VectorItt<T>(&container[0]);
+	return vitt;
+}
+
+template <typename T>
+typename Vector<T>::iterator Vector<T>::end() const{
+	VectorItt<T> vitt = VectorItt<T>(&container[sze]);
+	return vitt;
+}
+
+template <typename T>
+size_t Vector<T>::sizee() const noexcept{
+	return sze;
+}
+
+template <typename T>
+size_t Vector<T>::capacity() const noexcept{
+	return cpsty;
+}
+
+template<typename T>
+void Vector<T>::shrink_to_fit() {
+	if (cpsty != sze) {
+		T* cont = new T[sze];
+		int i = 0;
+		for (T* tmpp = container; tmpp < container + sze; ++tmpp) {
+			*(cont + i) = *tmpp;
+			i++;
+		}
+		delete[] container;
+		container = cont;
+		cpsty = sze;
+	//	invar();
+	}
+}
+
+template<typename T>
+void Vector<T>::push_back(T c) {
+	invar();
+	++sze;
+	if (sze > cpsty) {
+		cpsty = cpsty + cpsty;
+		T* cont = new T[cpsty];
+		int i = 0;
+		for (T* tmpp = cont; tmpp != cont + sze; ++tmpp) {
+			*tmpp = *(container + i);
+			i++;
+		}
+		cont[sze - 1] = c;
+		delete[] container;
+		container = cont;
+	} else {
+		container[sze - 1] = c;
+	}
+	invar();
+}
+
+template <typename T>
+void Vector<T>::resize(size_t n){
+	if (n > cpsty) {
+		T* cont = new T[n];
+		int i = 0;
+		for (T* tmpp = container; tmpp < container + sze; ++tmpp) {
+			*(cont + i) = *tmpp;
+			i++;
+		}
+		for (T* tmpp = container + sze; tmpp < container + n; ++tmpp) {
+			*(cont + i) = T();
+			i++;
+		}
+		delete[] container;
+		container = cont;
+		cpsty = n;
+		sze = n;
+		invar();
+	}else if(n > sze){
+		for (T* tmpp = container + sze; tmpp < container + n; ++tmpp) {
+			*tmpp = T();
+		}
+		sze = n;
+		invar();
+	}else if(n < sze){
+		sze = n;
+		invar();
+	}
+}
+
 
 #endif /* VECTOR_H_ */
--- src/VectorItt.h
+++ src/VectorItt.h
@@ -1,24 +1,24 @@
 /*
  * VectorItt.h
  *
- *  Created on: Nov 21, 2018
- *      Author: xps
+ *  Created on: Dec 3, 2018
+ *      Author: Oscar Arréhn Ågren
  */
 
 #ifndef VECTORITT_H_
 #define VECTORITT_H_
-
 #include <iostream>
 
-template<class T> class VectorItt {
 
+template<class T> class VectorItt {
+public:
 	typedef T value_type;
 	typedef T& reference;
 	typedef std::random_access_iterator_tag iterator_category;
 	typedef signed int difference_type;
 	typedef T* pointer;
+	pointer ptr;
 
-public:
 	VectorItt();
 	virtual ~VectorItt();
 	VectorItt(T* p);
@@ -34,12 +34,129 @@ public:
 	VectorItt operator+(difference_type i) const;
 	VectorItt operator-(difference_type i) const;
 	difference_type operator-(const VectorItt& other) const;
-	friend bool operator==(const VectorItt& lhs, const VectorItt& rhs);
-	friend bool operator!=(const VectorItt& lhs, const VectorItt& rhs);
-	friend bool operator<(const VectorItt& lhs, const VectorItt& rhs);
-	friend bool operator>(const VectorItt& lhs, const VectorItt& rhs);
-	friend bool operator<=(const VectorItt& lhs, const VectorItt& rhs);
-	friend bool operator>=(const VectorItt& lhs, const VectorItt& rhs);
+	template<class TT> friend bool operator==(const VectorItt& lhs, const VectorItt& rhs);
+	template<class TT> friend bool operator!=(const VectorItt& lhs, const VectorItt& rhs);
+	template<class TT> friend bool operator<(const VectorItt& lhs, const VectorItt& rhs);
+	template<class TT> friend bool operator>(const VectorItt& lhs, const VectorItt& rhs);
+	template<class TT> friend bool operator<=(const VectorItt& lhs, const VectorItt& rhs);
+	template<class TT> friend bool operator>=(const VectorItt& lhs, const VectorItt& rhs);
 };
+
+
+/*****************************************************************************************/
+
+template<class T>
+VectorItt<T>::VectorItt() {
+	ptr = nullptr;
+}
+
+template<class T>
+VectorItt<T>::VectorItt(T* p){
+	ptr = p;
+}
+
+template<class T>
+VectorItt<T>::VectorItt(const VectorItt& other) {
+	ptr = other.ptr;
+}
+
+template<class T>
+VectorItt<T>& VectorItt<T>::operator= (const VectorItt& other){
+	this->ptr = other.ptr;
+	return *this;
+}
+
+template<class T>
+T& VectorItt<T>::operator*(){
+	return *ptr;
+}
+
+template<class T>
+T* VectorItt<T>::operator ->(){
+	return ptr;
+}
+
+template<class T>
+T& VectorItt<T>::operator[](size_t i){
+	return *(ptr + i);
+}
+
+template<class T>
+VectorItt<T>& VectorItt<T>::operator++(){
+	++ptr;
+	return *this;
+}
+
+template<class T>
+VectorItt<T>& VectorItt<T>::operator--(){
+	--ptr;
+	return *this;
+}
+
+template<class T>
+VectorItt<T> VectorItt<T>::operator++(int){
+	VectorItt<T> tmp = *this;
+	++ptr;
+	return tmp;
+}
+
+template<class T>
+VectorItt<T> VectorItt<T>::operator--(int){
+	VectorItt<T> tmp = *this;
+	--ptr;
+	return tmp;
+}
+
+template<class T>
+VectorItt<T> VectorItt<T>::operator+(difference_type i) const{
+	return VectorItt<T>(ptr + i);
+}
+
+template<class T>
+VectorItt<T> VectorItt<T>::operator-(difference_type i) const{
+	return VectorItt<T>(ptr - i);
+}
+
+template<class T>
+typename VectorItt<T>::difference_type VectorItt<T>::operator-(const VectorItt& other) const{
+	difference_type val = ptr - other.ptr;
+	return val;
+}
+
+template<class T>
+bool operator==(const VectorItt<T>& lhs, const VectorItt<T>& rhs){
+	return lhs.ptr == rhs.ptr;
+}
+
+template<class T>
+bool operator!=(const VectorItt<T>& lhs, const VectorItt<T>& rhs){
+	return lhs.ptr != rhs.ptr;
+}
+
+template<class T>
+bool operator<(const VectorItt<T>& lhs, const VectorItt<T>& rhs){
+	return lhs.ptr < rhs.ptr;
+}
+
+template<class T>
+bool operator>(const VectorItt<T>& lhs, const VectorItt<T>& rhs){
+	return lhs.ptr > rhs.ptr;
+}
+
+template<class T>
+bool operator<=(const VectorItt<T>& lhs, const VectorItt<T>& rhs){
+	return lhs.ptr <= rhs.ptr;
+}
+
+template<class T>
+bool operator>=(const VectorItt<T>& lhs, const VectorItt<T>& rhs){
+	return lhs.ptr >= rhs.ptr;
+}
+
+template<class T>
+VectorItt<T>::~VectorItt() {
+	//Do nothing.
+}
+
 
 #endif /* VECTORITT_H_ */
